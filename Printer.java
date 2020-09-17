da4b862cab547c4144d6b7da85ab2cdef0d4bd17
Kevin xiao <60145956+unusedusername1@users.noreply.github.com>
2020-09-17 09:42:27 -0400

    Update Printer.java
    removed comments

--- Printer.java
+++ Printer.java
@@ -1,31 +1,12 @@
-//ReceiptPrinter.java
-
-/* This is a java file 'stub' or piece of a file designed to get you started on      
-* learning to code in java, while at the same time, exploring a little more about
-* collaboration, version control and multiple file scenarios.
-*
-*For this assignment, I would like you to fork, modify and commit this file, while
-* adding the following elements to your code.
-*
-* The receipt printer file could take arguments or parametrs from another class
-* This file will be purely for output
-****************************************
-* 40 characters is a standard width    * 
-* for typical receipt paper, your task *
-* is to make output from the screen    *
-* look like it is printing to a small  *
-* receipt type printer, with all the   *
-* text formatted to 40 characters wide *
-*                                      *
-* Have fun with the assignment!!       *
-****************************************
-*/
-
+//kevin xiao sept 2020
 public class Printer {
 
 public void print(String txt){
-
-
+   //manages 1 line of text
+   int n = txt.length();
+   System.out.print("*    "); System.out.print(txt); 
+   for(int i = 0; i < 34-n; i++) System.out.print(" "); //insert space until line length is 40
+   System.out.println("*");
 
 }
 
